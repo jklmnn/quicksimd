bfa80814d4297ff6be55408d57b2badc16cf38d4
Johannes Kliemann <jk@jkliemann.de>
2018-04-27 14:31:54 +0200

    add package info

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'quicksimd',
+        version = '0.0.1',
+        description = 'Convenience package for easy simd processing',
+        url = 'https://github.com/jklmnn/quicksimd',
+        author = 'Johannes Kliemann',
+        author_email = 'jk@jkliemann.de',
+        license = 'GPLv3',
+        packages = ['quicksimd'],
+        zip_safe = False)
